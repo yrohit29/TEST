dfc536dc184b6d237f448ea3586201a46723ae73
yrohit29 <103255493+yrohit29@users.noreply.github.com>
2023-05-11 16:55:19 +0530

    Add files via upload

---
+++ Do while.ps1
@@ -0,0 +1,51 @@
+﻿$i=1
+do{
+New-Item c:\hp\Rohit$i -ItemType Directory
+$i++
+}while($i -le 10)
+
+$i=1
+do{
+New-Item c:\hp\Text$i -ItemType File
+$i++
+}while($i -le 10)
+
+$i=1
+do{
+    $j=1
+    do{
+        new-item c:\hp\Rohit$i\Yadav$j -ItemType Directory
+        $j++
+    }while($j -le 10)
+    $i++
+}while($i -le 10)
+
+$i=1
+do{
+    $j=1
+        do{
+            $k=1
+            do{
+            New-Item C:\hp\Rohit$i\Yadav$j\Test$k.txt -ItemType File
+            $k++
+        }while($k -le 10)
+     $j++
+}while($j -le 10)
+$i++
+}while($i -le 10)
+
+
+$i=1
+do
+{
+    $j=1
+        do
+            {
+            copy-item C:\hp\Text$i -Recurse -Destination c:\hp\Rohit$j
+            $j++
+}while($j -le 10)
+$i++
+}while($i -le 10)
+
+
+
---
+++ while loop.ps1
@@ -0,0 +1,65 @@
+﻿$i=1
+
+while($i -le 10)
+{
+New-Item C:\dell\Rohit$i -ItemType Directory
+$i++
+}
+
+$i=1
+
+while($i -le 10)
+{
+New-Item C:\dell\Text$i.txt -ItemType File
+$i++
+}
+
+$i=1
+
+while($i -le 10)
+{
+$j=1
+while($j -le 10)
+{
+New-Item C:\dell\Rohit$i\Yadav$j -ItemType Directory
+$j++
+}
+
+$i++
+}
+
+$i=1
+while($i -le 10){
+    $j=1
+    while($j -le 10){
+        $k=1
+        while($k -le 10){
+            New-Item C:\dell\Rohit$i\Yadav$j\Sangli$k.txt -ItemType File
+            $k++
+            }
+        $j++
+     }
+    $i++
+}
+
+$i=1
+while($i -le 10){
+    $j=1
+    while($j -le 10){
+        Copy-Item C:\dell\Text$i.txt -Recurse -Destination C:\dell\Rohit$j
+        $j++
+   
+ 
+ }
+$i++
+}
+
+$i=1
+while($i -le 10){
+    $j=1
+    while($j -le 10){
+        Remove-Item C:\dell\Rohit$i\Text$j.txt 
+        $j++}
+$i++
+}
+
